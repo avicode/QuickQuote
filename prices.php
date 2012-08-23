162fab5ce7026fa047b67437ed0b2a5f3376f403
Zach Ross-Clyne <zach@avicode.co.uk>
2012-08-23 16:52:24 +0100

    basic system written

---
+++ prices.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Prices for every item */
+$standard_website = 125;
+$flash_website = 250;
+$databases = 25;
+$passwords = 250;
+$payment = 500;
+
+?>
---
+++ quote.php
@@ -0,0 +1,48 @@
+<?php
+
+/* Include prices file */
+include('prices.php');
+
+$selected_website_type = $_POST['web_type'];
+$selected_databases = $_POST['databases'];
+$selected_password = $_POST['password'];
+$selected_payment = $_POST['payment'];
+
+$website_type_cost = 0;
+$website_database_cost = 0;
+$website_password_cost = 0;
+$website_payment_cost = 0;
+
+$total_cost = 0;
+
+if ($selected_website_type == "Standard") {
+	$website_type_cost = $standard_website;
+} else {
+	$website_type_cost = $flash_website;
+}
+
+$website_database_cost = $databases * $selected_databases;
+
+if ($selected_password == "on") {
+	$website_password_cost = $passwords;
+} else {
+	$website_password_cost = 0;
+}
+
+if ($selected_payment == "on") {
+	$website_payment_cost = $payment;
+} else {
+	$website_payment_cost = 0;
+}
+
+$total_cost = $website_database_cost + $website_password_cost + $website_type_cost + $website_payment_cost;
+
+
+
+?>
+
+<?php 
+
+echo "<b>".$total_cost."</b>";
+
+?>
